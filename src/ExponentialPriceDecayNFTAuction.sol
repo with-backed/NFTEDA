658e0c331c83820625c94dd2067e1131dc8721c6
Wilson Cusack <cusackw@gmail.com>
2022-09-13 16:20:53 -0400

    need to test more

--- src/ExponentialPriceDecayNFTAuction.sol
+++ src/ExponentialPriceDecayNFTAuction.sol
@@ -5,14 +5,12 @@ import {ERC721} from "solmate/tokens/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
-import {FullMath} from "fullrange/libraries/FullMath.sol";
-import {TickMath} from "fullrange/libraries/TickMath.sol";
-
 contract ExponentialPriceDecayNFTAuction {
     struct Auction {
         uint256 auctionAssetID;
         ERC721 auctionAssetContract;
-        uint256 perPeriodDecay;
+        // FixedPointMathLib.WAD = 100%
+        uint256 perPeriodDecayPercentWad;
         uint256 secondsInPeriod;
         uint256 startPrice;
         ERC20 paymentAsset;
@@ -22,7 +20,7 @@ contract ExponentialPriceDecayNFTAuction {
         uint256 indexed auctionID,
         uint256 indexed auctionAssetID,
         ERC721 indexed auctionAssetContract,
-        uint256 perPeriodDecay,
+        uint256 perPeriodDecayPercentWad,
         uint256 secondsInPeriod,
         uint256 startPrice,
         ERC20 paymentAsset
@@ -41,7 +39,7 @@ contract ExponentialPriceDecayNFTAuction {
             id,
             auction.auctionAssetID,
             auction.auctionAssetContract,
-            auction.perPeriodDecay,
+            auction.perPeriodDecayPercentWad,
             auction.secondsInPeriod,
             auction.startPrice,
             auction.paymentAsset
@@ -50,8 +48,9 @@ contract ExponentialPriceDecayNFTAuction {
 
     error InvalidAuction();
     error InsufficientPayment();
+    error MaxPriceTooLow();
 
-    function purchaseNFT(Auction calldata auction, address sendTo, bytes calldata data) external {
+    function purchaseNFT(Auction calldata auction, uint256 maxPrice, bytes calldata data) external {
         uint256 id = auctionID(auction);
         uint256 startTime = auctionStartTime[id];
 
@@ -61,38 +60,43 @@ contract ExponentialPriceDecayNFTAuction {
 
         uint256 beforeBalance = auction.paymentAsset.balanceOf(address(this));
         uint256 price = currentPrice(auction);
+
+        // price changes over time and the price the caller pays
+        // will depend on when their tx is included in a block
+        // passing a max price offers a nice sanity check on
+        // what you pay
+        if (price > maxPrice) {
+            revert MaxPriceTooLow();
+        }
+
         delete auctionStartTime[id];
-        auction.auctionAssetContract.safeTransferFrom(address(this), sendTo, auction.auctionAssetID, data);
+
+        // We effectively use this as a callback, via the on receive handler,
+        // allowing the buyer to receive the NFT first and then provide payment,
+        // meaning they could sell the NFT in some arb to provide payment.
+        // TBD if we should have a dedicated callback method that callers should implement.
+        auction.auctionAssetContract.safeTransferFrom(address(this), msg.sender, auction.auctionAssetID, data);
 
         if (auction.paymentAsset.balanceOf(address(this)) - price < beforeBalance) {
             revert InsufficientPayment();
         }
+
+        emit EndAuction(id);
     }
 
     function currentPrice(Auction calldata auction) public view returns (uint256) {
         uint256 secondsElapsed = block.timestamp - auctionStartTime[auctionID(auction)];
         uint256 ratio = FixedPointMathLib.divWadDown(secondsElapsed, auction.secondsInPeriod);
-        int256 price = int256(auction.startPrice)
-            * FixedPointMathLib.powWad(int256(FixedPointMathLib.WAD - auction.perPeriodDecay), int256(ratio));
-        return (uint256(price) / FixedPointMathLib.WAD);
+        uint256 percentWadRemainingPerPeriod = FixedPointMathLib.WAD - auction.perPeriodDecayPercentWad;
+        int256 multiplier = FixedPointMathLib.powWad(toInt256(percentWadRemainingPerPeriod), toInt256(ratio));
+        // casting to uint256 is safe because percentWadRemainingPerPeriod is non negative
+        uint256 price = auction.startPrice * uint256(multiplier);
+        return (price / FixedPointMathLib.WAD);
     }
 
-    function currentPriceUniswap(Auction calldata auction) public returns (uint256) {
-        int256 secondsPassed = int256(block.timestamp - auctionStartTime[auctionID(auction)]);
-        // manual for now, should compute and save on auction creation
-        int24 endTick = -46055;
-        unchecked {
-            uint256 sqrtPrice =
-                TickMath.getSqrtRatioAtTick(int24(endTick * secondsPassed / int256(auction.secondsInPeriod)));
-            return FullMath.mulDiv(auction.startPrice, sqrtPrice, 1 << 96);
-        }
-    }
-
-    function _endTick(uint256 perPeriodDecay) internal returns (int24) {
-        uint256 remaining = FixedPointMathLib.WAD - perPeriodDecay;
-        unchecked {
-            return TickMath.getTickAtSqrtRatio(uint160((remaining * (1 << 96)) / FixedPointMathLib.WAD));
-        }
+    function toInt256(uint256 y) internal pure returns (int256 z) {
+        require(y < 2 ** 255);
+        z = int256(y);
     }
 
     function auctionID(Auction calldata auction) public pure returns (uint256) {
--- test/ExponentialPriceDecayNFTAuction.t.sol
+++ test/ExponentialPriceDecayNFTAuction.t.sol
@@ -5,8 +5,6 @@ import "forge-std/Test.sol";
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
-import {FullMath} from "fullrange/libraries/FullMath.sol";
-import {TickMath} from "fullrange/libraries/TickMath.sol";
 
 import "../src/ExponentialPriceDecayNFTAuction.sol";
 
@@ -19,7 +17,7 @@ contract ExponentialPriceDecayNFTAuctionTest is Test {
         ExponentialPriceDecayNFTAuction.Auction memory auction = ExponentialPriceDecayNFTAuction.Auction({
             auctionAssetID: 0,
             auctionAssetContract: ERC721(address(0)),
-            perPeriodDecay: uint256(0.9e18),
+            perPeriodDecayPercentWad: uint256(0.9e18),
             secondsInPeriod: 1 days,
             startPrice: 1e18,
             paymentAsset: ERC20(address(0))
@@ -27,8 +25,7 @@ contract ExponentialPriceDecayNFTAuctionTest is Test {
 
         auctionHouse.startAuction(auction);
         vm.warp(block.timestamp + 1 days);
-        emit log_named_uint("Precise", 1e17);
-        emit log_named_uint("Solmate", auctionHouse.currentPrice(auction));
-        emit log_named_uint("Uniswap", auctionHouse.currentPriceUniswap(auction));
+        // off by 1, precise 1e17
+        assertEq(auctionHouse.currentPrice(auction), 99999999999999999);
     }
 }
