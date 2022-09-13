eb22bce9baf7531a31a96340fdfd7fff67518a1f
Wilson Cusack <cusackw@gmail.com>
2022-09-13 15:50:00 -0400

    checkin, uniswap functions present

--- src/ExponentialPriceDecayNFTAuction.sol
+++ src/ExponentialPriceDecayNFTAuction.sol
@@ -5,11 +5,15 @@ import {ERC721} from "solmate/tokens/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
+import {FullMath} from "fullrange/libraries/FullMath.sol";
+import {TickMath} from "fullrange/libraries/TickMath.sol";
+
 contract ExponentialPriceDecayNFTAuction {
     struct Auction {
         uint256 auctionAssetID;
         ERC721 auctionAssetContract;
-        uint256 perSecondDecayWAD;
+        uint256 perPeriodDecay;
+        uint256 secondsInPeriod;
         uint256 startPrice;
         ERC20 paymentAsset;
     }
@@ -18,7 +22,8 @@ contract ExponentialPriceDecayNFTAuction {
         uint256 indexed auctionID,
         uint256 indexed auctionAssetID,
         ERC721 indexed auctionAssetContract,
-        uint256 perSecondDecayWAD,
+        uint256 perPeriodDecay,
+        uint256 secondsInPeriod,
         uint256 startPrice,
         ERC20 paymentAsset
     );
@@ -36,15 +41,17 @@ contract ExponentialPriceDecayNFTAuction {
             id,
             auction.auctionAssetID,
             auction.auctionAssetContract,
-            auction.perSecondDecayWAD,
+            auction.perPeriodDecay,
+            auction.secondsInPeriod,
             auction.startPrice,
             auction.paymentAsset
             );
     }
 
     error InvalidAuction();
+    error InsufficientPayment();
 
-    function purchaseNFT(Auction calldata auction) external {
+    function purchaseNFT(Auction calldata auction, address sendTo, bytes calldata data) external {
         uint256 id = auctionID(auction);
         uint256 startTime = auctionStartTime[id];
 
@@ -53,13 +60,39 @@ contract ExponentialPriceDecayNFTAuction {
         }
 
         uint256 beforeBalance = auction.paymentAsset.balanceOf(address(this));
+        uint256 price = currentPrice(auction);
+        delete auctionStartTime[id];
+        auction.auctionAssetContract.safeTransferFrom(address(this), sendTo, auction.auctionAssetID, data);
+
+        if (auction.paymentAsset.balanceOf(address(this)) - price < beforeBalance) {
+            revert InsufficientPayment();
+        }
     }
 
     function currentPrice(Auction calldata auction) public view returns (uint256) {
         uint256 secondsElapsed = block.timestamp - auctionStartTime[auctionID(auction)];
+        uint256 ratio = FixedPointMathLib.divWadDown(secondsElapsed, auction.secondsInPeriod);
         int256 price = int256(auction.startPrice)
-            * FixedPointMathLib.powWad(int256(auction.perSecondDecayWAD), int256(secondsElapsed));
+            * FixedPointMathLib.powWad(int256(FixedPointMathLib.WAD - auction.perPeriodDecay), int256(ratio));
         return (uint256(price) / FixedPointMathLib.WAD);
+    }
+
+    function currentPriceUniswap(Auction calldata auction) public returns (uint256) {
+        int256 secondsPassed = int256(block.timestamp - auctionStartTime[auctionID(auction)]);
+        // manual for now, should compute and save on auction creation
+        int24 endTick = -46055;
+        unchecked {
+            uint256 sqrtPrice =
+                TickMath.getSqrtRatioAtTick(int24(endTick * secondsPassed / int256(auction.secondsInPeriod)));
+            return FullMath.mulDiv(auction.startPrice, sqrtPrice, 1 << 96);
+        }
+    }
+
+    function _endTick(uint256 perPeriodDecay) internal returns (int24) {
+        uint256 remaining = FixedPointMathLib.WAD - perPeriodDecay;
+        unchecked {
+            return TickMath.getTickAtSqrtRatio(uint160((remaining * (1 << 96)) / FixedPointMathLib.WAD));
+        }
     }
 
     function auctionID(Auction calldata auction) public pure returns (uint256) {
--- test/ExponentialPriceDecayNFTAuction.t.sol
+++ test/ExponentialPriceDecayNFTAuction.t.sol
@@ -5,55 +5,30 @@ import "forge-std/Test.sol";
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {FullMath} from "fullrange/libraries/FullMath.sol";
+import {TickMath} from "fullrange/libraries/TickMath.sol";
 
 import "../src/ExponentialPriceDecayNFTAuction.sol";
 
 contract ExponentialPriceDecayNFTAuctionTest is Test {
     ExponentialPriceDecayNFTAuction public auctionHouse = new ExponentialPriceDecayNFTAuction();
 
-    function setUp() public {
-    }
+    function setUp() public {}
 
     function testCurrentPrice() public {
         ExponentialPriceDecayNFTAuction.Auction memory auction = ExponentialPriceDecayNFTAuction.Auction({
             auctionAssetID: 0,
             auctionAssetContract: ERC721(address(0)),
-            perSecondDecayWAD: uint256(0.1e17) / 1 days,
+            perPeriodDecay: uint256(0.9e18),
+            secondsInPeriod: 1 days,
             startPrice: 1e18,
             paymentAsset: ERC20(address(0))
         });
-        emit log_int(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)));
-        emit log_int(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)) * 1 days);
-        emit log_int(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)) * 1 days / 1e18);
-        emit log_int(FixedPointMathLib.expWad(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)) * int256(1 days) / int256(1e18)));
-        emit log_int(int256(1e18) * (FixedPointMathLib.expWad(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)) * 1 days / 1e18)));
-        emit log_int(int256(1e18) * (FixedPointMathLib.expWad(FixedPointMathLib.lnWad(int256(auction.perSecondDecayWAD)) * 2 days / 1e18)) / 1e19);
-        emit log_string("FixedPointMathLib.powWad(2, 50)");
-        emit log_string("=>");
-        emit log_int(FixedPointMathLib.powWad(2, 50));
-        emit log_string("FixedPointMathLib.powWad(50, 50)");
-        emit log_string("=>");
-        emit log_int(FixedPointMathLib.powWad(50, 50));
-        emit log_string("FixedPointMathLib.powWad(0.9e18, 100)");
-        emit log_string("=>");
-        emit log_int(2e18 * FixedPointMathLib.powWad(1, 10) / 1e18);
 
-        uint256 ratio = FixedPointMathLib.divWadDown(30, 1 days);
-        uint256 change = 0.1e18;// * ratio / 1e18;
-        emit log_uint(change);
-        // emit log_int(2e18 * FixedPointMathLib.powWad(int256(change), int256(ratio)) / 1e18);
-        // emit log_int(FixedPointMathLib.powWad(2e20, 4) / 1e18);
-        // emit log_uint(FixedPointMathLib.rpow(90, 1 days, 100));
-        // emit log_uint(auction.perSecondDecayWAD);
-        // emit log_int(FixedPointMathLib.expWad(1));
-        // emit log_int(1e18 * FixedPointMathLib.powWad(int256(auction.perSecondDecayWAD), 1) / 1e18);
-        // emit log_int(FixedPointMathLib.powWad(int256(auction.perSecondDecayWAD), 2 ));
-        // emit log_int(FixedPointMathLib.powWad(int256(auction.perSecondDecayWAD), 10 ));
-        // emit log_uint(FixedPointMathLib.mulWadUp(1e18, uint256(FixedPointMathLib.powWad(int256(auction.perSecondDecayWAD), 10 days))));
-        // auctionHouse.startAuction(auction);
-        // emit log_uint(auctionHouse.currentPrice(auction));
-        // vm.warp(block.timestamp + 1 days);
-        // emit log_uint(auctionHouse.currentPrice(auction));
-
+        auctionHouse.startAuction(auction);
+        vm.warp(block.timestamp + 1 days);
+        emit log_named_uint("Precise", 1e17);
+        emit log_named_uint("Solmate", auctionHouse.currentPrice(auction));
+        emit log_named_uint("Uniswap", auctionHouse.currentPriceUniswap(auction));
     }
 }
