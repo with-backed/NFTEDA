c445f44e8d604ecf2937d9123887170206e00e54
Wilson Cusack <cusackw@gmail.com>
2022-09-12 21:37:35 -0400

    progress

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract ExponentialPriceDecayNFTAuction {
+    struct Auction {
+        uint256 auctionAssetID;
+        ERC721 auctionAssetContract;
+        uint256 perSecondDecayWAD;
+        uint256 startPrice;
+        ERC20 paymentAsset;
     }
 
-    function increment() public {
-        number++;
+    event StartAuction(
+        uint256 indexed auctionID,
+        uint256 indexed auctionAssetID,
+        ERC721 indexed auctionAssetContract,
+        uint256 perSecondDecayWAD,
+        uint256 startPrice,
+        ERC20 paymentAsset
+    );
+
+    event EndAuction(uint256 indexed auctionID);
+
+    mapping(uint256 => uint256) auctionStartTime;
+
+    function startAuction(Auction calldata auction) external returns (uint256 id) {
+        id = auctionID(auction);
+
+        auctionStartTime[id] = block.timestamp;
+
+        emit StartAuction(id, auction.auctionAssetID, auction.auctionAssetContract, auction.perSecondDecayWAD, auction.startPrice, auction.paymentAsset);
+    }
+
+    error InvalidAuction();
+
+    function purchaseNFT(Auction calldata auction) external {
+        uint256 id = auctionID(auction);
+        uint256 startTime = auctionStartTime[id];
+
+        if (startTime == 0){
+            revert InvalidAuction();
+        }
+
+        uint256 beforeBalance = auction.paymentAsset.balanceOf(address(this));
+
+        
+
+    }
+
+    function currentPrice(Auction calldata auction) pure public returns (uint256) {
+        return auction.startPrice
+        * auction.perSecondDecayWAD
+    }
+
+    function auctionID(Auction calldata auction) pure public returns (uint256) {
+        return uint256(keccak256(abi.encode(auction)));
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,23 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Counter.sol";
+// import "forge-std/Test.sol";
+// import "../src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
-    function setUp() public {
-       counter = new Counter();
-       counter.setNumber(0);
-    }
+// contract CounterTest is Test {
+//     Counter public counter;
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+//     function setUp() public {
+//         counter = new Counter();
+//         counter.setNumber(0);
+//     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
-}
+//     function testIncrement() public {
+//         counter.increment();
+//         assertEq(counter.number(), 1);
+//     }
+
+//     function testSetNumber(uint256 x) public {
+//         counter.setNumber(x);
+//         assertEq(counter.number(), x);
+//     }
+// }
